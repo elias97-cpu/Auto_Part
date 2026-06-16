b2234fe999cbb8a246ed448229190938f06020db
Elias <Eliasdebalke97@gmail.com>
2026-06-16 22:08:59 +0300

    Add Main entry point class

---
+++ src/com/autoparts/Main.java
@@ -0,0 +1,13 @@
+package com.autoparts;
+
+import com.autoparts.ui.ConsoleUI;
+
+/**
+ * Main entry point for the Auto Parts Link Ethiopia application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConsoleUI ui = new ConsoleUI();
+        ui.start();
+    }
+}
